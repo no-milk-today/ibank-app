17d6aa531226617c7104f883171e971327057fb7
no-milk-today <drmhere@yandex.by>
2025-07-09 21:43:48 +0200

    #fraud initial, add .env file (for docker)

--- customer/src/main/java/com/practice/drm/customer/CustomerController.java
+++ customer/src/main/java/com/practice/drm/customer/CustomerController.java
@@ -1,18 +1,20 @@
 package com.practice.drm.customer;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("api/v1/customoers")
+@RequestMapping("api/v1/customers")
 public record CustomerController(CustomerService customerService) {
     @PostMapping
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
         log.info("new customer registration {}", customerRegistrationRequest);
         customerService.registerCustomer(customerRegistrationRequest);
+    }
+
+    @GetMapping("/public")
+    public String getCustomer() {
+        return "Customers public endpoint";
     }
 }
---
+++ customer/src/main/java/com/practice/drm/customer/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.practice.drm.customer.security;
+
+//@Configuration // Uncomment this line if you want to use this class as a configuration class
+//@EnableMethodSecurity
+public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("api/v1/customers/public/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .oauth2ResourceServer(oauth2 -> oauth2
+//                        .jwt(Customizer.withDefaults())  // JWT Resource Server
+//                );
+//
+//        return http.build();
+//    }
+}
