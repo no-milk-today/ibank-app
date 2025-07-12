409b47e2e36486b92d7c5f856a4adb24c9be7d83
no-milk-today <drmhere@yandex.by>
2025-07-12 17:39:47 +0200

    # Circuit Breaker pattern (making use of resilience4j)

--- customer/src/main/java/com/practice/drm/customer/CustomerService.java
+++ customer/src/main/java/com/practice/drm/customer/CustomerService.java
@@ -1,18 +1,30 @@
 package com.practice.drm.customer;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(
-        CustomerRepository customerRepository,
-        RestTemplate restTemplate
-) {
+@Slf4j
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
+
+    public CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
+        this.customerRepository = customerRepository;
+        this.restTemplate = restTemplate;
+    }
+
+    @CircuitBreaker(name = "fraudCheckService", fallbackMethod = "fraudCheckFallback")
+    @Retry(name = "fraudCheckService")
     public void registerCustomer(CustomerRegistrationRequest request) {
         var customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
-                .firstName(request.email())
+                .email(request.email())
                 .build();
         // todo: check if email valid
         // todo: check if not taken
@@ -30,5 +42,11 @@ public record CustomerService(
             throw new IllegalStateException("fraudster");
         }
         // todo: send a notification
+    }
+
+    public void fraudCheckFallback(CustomerRegistrationRequest request, Throwable ex) {
+        log.error("Fraud check service is unavailable. Fallback method invoked for request: {}", request, ex);
+        // Логика fallback: например, логирование, сохранение статуса, отправка уведомления
+        throw new IllegalStateException("Fraud service unavailable. Please try later.", ex);
     }
 }
