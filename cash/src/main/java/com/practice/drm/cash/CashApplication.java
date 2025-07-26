a2a73dbb21ba94b6458381fbfaa19290c2474a7e
no-milk-today <drmhere@yandex.by>
2025-07-26 10:50:11 +0200

    #cash intiail

---
+++ cash/src/main/java/com/practice/drm/cash/CashApplication.java
@@ -0,0 +1,13 @@
+package com.practice.drm.cash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CashApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CashApplication.class, args);
+    }
+}
