d0b79d53249817193461f7d54ca9379fa5edc8e1
no-milk-today <drmhere@yandex.by>
2025-07-29 18:13:04 +0200

    #add cash tests (with security)

--- cash/src/main/java/com/practice/drm/cash/config/FeignConfig.java
+++ cash/src/main/java/com/practice/drm/cash/config/FeignConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
+// https://www.edstem.com/blog/jwt-tokens-microservice-architectures-spring-cloud/
 @Configuration
 public class FeignConfig {
 
