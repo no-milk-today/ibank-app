b354da31b1a1e075dae576868391083ecf397e41
viktar-b <drmhere@yandex.by>
2025-09-27 14:10:02 +0300

    #Disabling Eureka for customer, notification, fraud

--- notification/src/main/java/com/practice/drm/notification/NotificationApplication.java
+++ notification/src/main/java/com/practice/drm/notification/NotificationApplication.java
@@ -3,9 +3,14 @@ package com.practice.drm.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@PropertySources({
+        @PropertySource("classpath:clients-default.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
